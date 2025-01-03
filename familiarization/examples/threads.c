ee863e04e98a8cc5d8eb81de57b13cafe61c2dec
come_ill_foo <rayveleaveme34@inbox.ru>
2025-01-03 12:59:38 +0000

    refactor(familiarization): add facade softlinks and folders for convenient structure

---
+++ familiarization/examples/threads.c
@@ -0,0 +1,23 @@
+// Copied from spinroot.com/modex/MANUAL.html
+int shared = 0;
+int *ptr;
+
+void thread1(void)
+{
+    int tmp;
+    ptr = &shared;
+    tmp = shared;
+    tmp++;
+    shared = tmp;
+}
+
+void thread2(void)
+{
+    int tmp;
+    if (ptr) {
+        tmp = shared;
+        tmp++;
+        shared = tmp;
+        assert(shared == 1);
+    }
+}
